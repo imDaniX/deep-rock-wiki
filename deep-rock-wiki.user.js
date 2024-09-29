eb417ac3e0fa8db00d62b358c06e2afcf019768e
Daniil Zao. <idanix@list.ru>
2024-09-30 01:39:59 +0300

    Copy hashtag too

--- deep-rock-wiki.user.js
+++ deep-rock-wiki.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         deep-rock-wiki
 // @namespace    https://github.com/imDaniX
-// @version      1.0.1
+// @version      1.0.2
 // @description  Transform https://deeprockgalactic.wiki.gg pages into https://deeprock.wiki link
 // @author       imDaniX
 // @homepageURL  https://github.com/imDaniX/deep-rock-wiki
@@ -12,6 +12,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=deeprockgalactic.wiki.gg
 // ==/UserScript==
 
+const MAIN_PAGE = "/Deep_Rock_Galactic_Wiki";
+
 (function() {
     'use strict';
 
@@ -19,7 +21,10 @@
         let selection = window.getSelection();
         if (!selection || selection.toString() == "") {
             let path = window.location.pathname.replace("/wiki", "");
-            event.clipboardData.setData("text/plain", "https://deeprock.wiki" + (path == "/Deep_Rock_Galactic_Wiki" ? "" : path));
+            if (path.endsWith(MAIN_PAGE)) path = path.substring(0, path.length - MAIN_PAGE.length);
+            let hash = window.location.hash;
+            if (hash && hash != "") path += hash;
+            event.clipboardData.setData("text/plain", "https://deeprock.wiki" + path);
             event.preventDefault();
         }
     });
